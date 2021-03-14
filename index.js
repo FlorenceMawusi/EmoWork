2479bf3ac960cf50dee4f882fa30b2d3e107021a
FlorenceMawusi <florenceoforixyz@gmail.com>
2021-03-14 09:36:44 +0000

    .

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use("/api/results", results);
 
 app.use("/", express.static(path.join(__dirname, "build")));
 
-app.get("/", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
